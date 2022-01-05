6ed5ac6cb6a35ea06bf54c477cd0c743ad20d817
hamdysaad20 <hamdysaad.biz@gmail.com>
2022-01-05 19:24:04 +0200

    max width

--- components/Main.js
+++ components/Main.js
@@ -6,53 +6,55 @@ import VideoPost from "./VideoPost"
 
 function Main() {
     return (
-        <div className=" lg:max-w-9xl text-center grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4">
+     <div className="fullscreen mx-auto justify-center text-center ">
+            <div className=" lg:max-w-9xl text-center grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-4">
           
-           <div className=" bg-gray-200 text-center hidden lg:inline-block overflow-hidden h-screen">
-                 <Sidinfo 
-                 imgUrl="/2.jpg"
-                 imgUrlCover="/ad300x250.png" 
-                 UserName="Hamdy Saad"
-                 UserBio="Frontend Web Developer"
-                 arcRead="0"
-                 arcWrote="0"
-                 arcViews="0"
-                 className=" h-screen top-2"
-                 />
-            </div>
-
-            <div  className=" col-span-2 scroll-m-0 overflow-scroll text-white px-2 h-screen ">
-
-               <div className="">
-               <BlogcompMain 
-                userImage="/2.jpg"
-                UserName="حمدي سعد"
-                PostDate="2"
-                PostTitle=" متبيلتم منتب  نمات  بعلبعلهخب"
+          <div className=" bg-gray-200 text-center hidden lg:inline-block overflow-hidden h-screen">
+                <Sidinfo 
+                imgUrl="/2.jpg"
+                imgUrlCover="/ad300x250.png" 
+                UserName="Hamdy Saad"
+                UserBio="Frontend Web Developer"
+                arcRead="0"
+                arcWrote="0"
+                arcViews="0"
+                className=" h-screen top-2"
                 />
-                <BlogPost
-                                userImage="/ad300x250.png"
-                                UserName="حمدي سعد"
-                                PostDate="2"
-                                PostTitle="بلتبمنلتبيملتمينتيبمكت متبيلتم منتب لتيبنملابكمل نمات نا بعلبعلهخب"
-                />
-                <VideoPost
-                            UserName="حمدي سعد"
-                            PostDate="2"
-                            PostTitle="بلتبمنلتبيملتمينتيبمكت متبيلتم منتب لتيبنملابكمل نمات نا بعلبعلهخب"
-                />
-               </div>
-            </div>
-            <div className=" mt-4  hidden  sm:inline-block bg-gray-200 h-screen overflow-hidden">
+           </div>
 
-                <Sidenav 
+           <div  className=" col-span-2 scroll-m-0 overflow-scroll text-white px-2 h-screen ">
+
+              <div className="">
+              <BlogcompMain 
+               userImage="/2.jpg"
+               UserName="حمدي سعد"
+               PostDate="2"
+               PostTitle=" متبيلتم منتب  نمات  بعلبعلهخب"
+               />
+               <BlogPost
+                               userImage="/ad300x250.png"
+                               UserName="حمدي سعد"
+                               PostDate="2"
+                               PostTitle="بلتبمنلتبيملتمينتيبمكت متبيلتم منتب لتيبنملابكمل نمات نا بعلبعلهخب"
+               />
+               <VideoPost
+                           UserName="حمدي سعد"
+                           PostDate="2"
+                           PostTitle="بلتبمنلتبيملتمينتيبمكت متبيلتم منتب لتيبنملابكمل نمات نا بعلبعلهخب"
+               />
+              </div>
+           </div>
+           <div className=" mt-4  hidden  sm:inline-block bg-gray-200 h-screen overflow-hidden">
+
+               <Sidenav 
+              
+              className=" h-screen top-2"
                
-               className=" h-screen top-2"
-                
-                />
-            
-            </div>
-        </div>
+               />
+           
+           </div>
+       </div>
+     </div>
     )
 }
 
