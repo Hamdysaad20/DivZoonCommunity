eeb3b74393019663b149f60b869a8e86217d874a
hamdysaad20 <hamdysaad.biz@gmail.com>
2022-03-10 03:05:47 +0200

    a

--- components/adslayout/AnnoumceHome.js
+++ components/adslayout/AnnoumceHome.js
@@ -3,9 +3,8 @@ import Image from 'next/image'
 
 function AnnoumceHome() {
   return (
-<div className="h-48 -mb-8 rounded-lg w-full bg-gray-300 relative ">
-<h1>frfgrg</h1>
- <Image  className="bg-red-500 absolute text-red-300 top-2 right-3  rounded-full" src="/ico/xico.png" alt="x" width="45" height="45"/>
+<div className="h-48 -mb-8 rounded-lg w-full bg-gray-600 relative ">
+ <Image  className="hover:bg-gray-100 duration-300  rounded-full" src="/ico/xico.png" alt="x" width="20" height="20"/>
  
 
 
